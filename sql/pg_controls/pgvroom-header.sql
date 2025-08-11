235555502fcf6c3bf2e1c767d739a9c4624f5a19
cvvergara <vicky@erosion.dev>
2025-08-11 11:36:09 -0600

    (sql) vrprouting -> pgvroom

--- sql/pg_controls/pgvroom-header.sql
+++ sql/pg_controls/pgvroom-header.sql
@@ -22,6 +22,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
  ********************************************************************PGR-GNU*/
-\echo Use "CREATE EXTENSION vrprouting" to load this file. \quit
+\echo Use "CREATE EXTENSION pgvroom" to load this file. \quit
 
 
